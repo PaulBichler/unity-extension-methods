c2df5067718cfe30a9806b42b7641835ccc7206a
Paul Bichler <d00242563@student.dkit.ie>
2023-04-24 21:55:12 +0200

    Added Child Layer Setter game object extension method
    - Added a GameObject extension method that sets the layer of the game object and all its children recursively.

--- Scripts/GameObjectExtensions.cs
+++ Scripts/GameObjectExtensions.cs
@@ -21,13 +21,13 @@ namespace UnityExtensionMethods
         }
         
         /// <summary>
-        /// Gets or adds the specified component to this game object.
+        /// Gets or adds the specified component to this GameObject.
         /// If the component already exists, it's returned.
         /// Otherwise, it adds a new component of the specified type and returns it.
         /// </summary>
         /// <typeparam name="T">The type of component to get or add.</typeparam>
-        /// <param name="gameObject">The game object to get or add the component to.</param>
-        /// <returns>The component of the specified type attached to the game object.</returns>
+        /// <param name="gameObject">The GameObject to get or add the component to.</param>
+        /// <returns>The component of the specified type attached to the GameObject.</returns>
         public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
         {
             if (gameObject.TryGetComponent<T>(out var component))
@@ -37,11 +37,11 @@ namespace UnityExtensionMethods
         }
         
         /// <summary>
-        /// Gets all components of the specified type in the direct children of this game object.
+        /// Gets all components of the specified type in the direct children of this GameObject.
         /// </summary>
         /// <typeparam name="T">The type of component to retrieve.</typeparam>
-        /// <param name="gameObject">The game object to search for the components.</param>
-        /// <returns>A list of all components of the specified type found in the direct children of this game object.</returns>
+        /// <param name="gameObject">The GameObject to search for the components.</param>
+        /// <returns>A list of all components of the specified type found in the direct children of this GameObject.</returns>
         public static List<T> GetComponentsInDirectChildren<T>(this GameObject gameObject) where T : Component
         {
             List<T> components = new List<T>();
@@ -56,11 +56,11 @@ namespace UnityExtensionMethods
         }
         
         /// <summary>
-        /// Destroys the specified component attached to this game object.
+        /// Destroys the specified component attached to this GameObject.
         /// If multiple component of the specified type are attached, only the first one is destroyed. 
         /// </summary>
         /// <typeparam name="T">The type of component to destroy.</typeparam>
-        /// <param name="gameObject">The game object that contains the component to destroy.</param>
+        /// <param name="gameObject">The GameObject that contains the component to destroy.</param>
         public static bool DestroyComponent<T>(this GameObject gameObject) where T : Component
         {
             if (!gameObject.TryGetComponent<T>(out var component)) 
@@ -68,6 +68,20 @@ namespace UnityExtensionMethods
             
             Object.Destroy(component);
             return true;
+        }
+        
+        /// <summary>
+        /// Sets the layer of this GameObject and all its children recursively.
+        /// </summary>
+        /// <param name="gameObject">The current GameObject.</param>
+        /// <param name="layer">The layer to set.</param>
+        public static void SetLayerRecursively(this GameObject gameObject, int layer)
+        {
+            gameObject.layer = layer;
+            foreach (Transform child in gameObject.transform)
+            {
+                child.gameObject.SetLayerRecursively(layer);
+            }
         }
     }
 }
